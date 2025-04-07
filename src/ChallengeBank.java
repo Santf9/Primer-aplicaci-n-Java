bf7ad3a3dcf4c548eeeaa56c6765cb6bfab53853
Santiago Ferreira <santiandresfer@hotmail.com>
2025-04-07 16:26:06 -0300

    Challenge Bank: Consultas de saldo y hacer depósitos

--- src/ChallengeBank.java
+++ src/ChallengeBank.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
+
 public class ChallengeBank {
     public static void main(String[] args) {
+        String nombreCliente = "Tony Stark";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 1599.99;
+        int opcion = 0;
+        Scanner teclado = new Scanner(System.in);
 
+        System.out.println("***********************");
+        System.out.println("\nNombre del cliente: " + nombreCliente);
+        System.out.println("Tipo de cuenta: " + tipoDeCuenta);
+        System.out.println("Saldo disponible: $" + saldo);
+        System.out.println("\n***********************");
+
+        String menu = """
+                ****Escriba la opción que desea realizar ****
+                1. Consultar saldo
+                2. Retirar dinero
+                3. Depositar dinero
+                4. Salir
+                """;
+
+        while (opcion != 4) {
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("\nSu saldo disponible es: $" + saldo);
+                    break;
+                case 2:
+                    System.out.println("¿Cuánto dinero desea retirar?");
+                    double valorARetirar = teclado.nextDouble();
+                    if (saldo < valorARetirar) {
+                        System.out.println("Saldo insifuciente");
+                    } else {
+                        saldo -= valorARetirar;
+                        System.out.println("\nSu nuevo saldo es: $" + saldo);
+                    }
+                    break;
+                case 3:
+                    System.out.println("¿Cuánto dinero desea depositar?");
+                    double valorADepositar = teclado.nextDouble();
+                    saldo += valorADepositar;
+                    System.out.println("\nSu saldo actualizado es: $" + saldo);
+                    break;
+                case 4:
+                    System.out.println("\nFinalizando operación... Gracias por usar el sistema. ¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Por favor, elija una opción válida.");
+            }
+        }
     }
 }
