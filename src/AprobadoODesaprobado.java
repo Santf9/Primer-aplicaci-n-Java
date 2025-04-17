c4153cd21c59055d199e99b16649b4773d3db6c0
Santiago Ferreira <santiandresfer@hotmail.com>
2025-04-17 18:15:02 -0300

    Práctica Condicionales

---
+++ src/AprobadoODesaprobado.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AprobadoODesaprobado {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la nota del estudiante: ");
+        double nota = scanner.nextDouble();
+
+        if (nota >= 7.0) {
+            System.out.println("El estudiante tuvo un promedio de " + nota + " y está aprobado.");
+        } else if (nota >= 5.0 && nota <= 6.9) {
+            System.out.println("El estudiante tuvo un promedio de " + nota + " y está en recuperación.");
+        } else {
+            System.out.println("El estudiante tuvo un promedio de " + nota + " y fue reprobado.");
+            
+        }
+    }
+}
---
+++ src/ParOImpar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ParOImpar {
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar al usuario que ingrese un número entero
+        System.out.println("Ingrese un número entero: ");
+        int numero = scanner.nextInt();
+
+        // Verificar si el número es par o impar con el operador % de módulo
+        if (numero % 2 == 0) {
+            System.out.println("El número " + numero + " es par.");
+        } else {
+            System.out.println("El número " + numero + " es impar.");
+        }
+        // Cerrar el escáner
+        scanner.close();
+    }
+}
